cd5e2d9152e4ff3c4420bfa9453b88970fdd5353
Pranat Siyal <pranatsiyal@gmail.com>
2025-01-03 16:13:06 -0600

    Lesson_2

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://dandrepairshop.com',
+    'https://www.dandrepairshop.com'
+]
+
+module.exports = allowedOrigins
--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const {logger} = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOption = require('./config/corsOption')
 const PORT = process.env.PORT || 3500 
 
+app.use(logger)
+
+app.use(cors(corsOption))
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use(cookieParser())
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
+
 app.all('*', (req, res) => {
     res.status(404)
     if (req.accepts('html')){
@@ -19,5 +34,6 @@ app.all('*', (req, res) => {
         res.type('txt').send('404 Not Found')
     }
 })
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
